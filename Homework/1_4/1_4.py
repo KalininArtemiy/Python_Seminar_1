15d8d5decf3f4716a27e65aa87c9b074ea284cbc
Kalinin <9776020@mail.ru>
2023-05-23 23:02:44 +0300

    HW 4 Fixed

--- Homework/1_4/1_4.py
+++ Homework/1_4/1_4.py
@@ -1,7 +1,7 @@
 n = int(input())
 m = n = int(input())
 k = n = int(input())
-if k < n * m and ((k / m == 0) or(k / n == 0)):
+if k < n * m and ((k % m == 0) or (k % n == 0)):
     print ('Yes')
 else:
     print ('No') 
